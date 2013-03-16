a5a9876329e8fc79618fdeb367ccaae54c8d2739
Tomi Toivio <tomi@seravo.fi>
2013-03-16 19:28:47 +0200

    Create wholesaler-plugin.php

---
+++ wholesaler-plugin.php
@@ -0,0 +1,26 @@
+<?php
+  /*
+    Plugin Name: WooCommerce Wholesale Plugin
+    Plugin URI: http://seravo.fi
+    Description: Experimental WooCommerce plugin for checking wholesaler stocks without a proper API
+    Version: 1.0
+    Author: Tomi Toivio
+    Author URI: http://seravo.fi
+    License: GPL2
+ */
+  /*  Copyright 2012 Tomi Toivio (email: tomi@seravo.fi)
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+?>
