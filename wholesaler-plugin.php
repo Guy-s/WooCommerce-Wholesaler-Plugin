9fbeb6431b684b0cd16cc2fb9dca174b788c2949
Tomi Toivio <tomi@seravo.fi>
2013-03-16 19:31:42 +0200

    Update wholesaler-plugin.php
    Importing code from old version.

--- wholesaler-plugin.php
+++ wholesaler-plugin.php
@@ -23,4 +23,48 @@
     along with this program; if not, write to the Free Software
     Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
+ 
+ /*
+        $curlurl = get_post_meta($this->id, 'curlurl', true);
+    		$curlpattern = get_post_meta($this->id, 'curlpattern', true);
+        $curlpatterntwo = get_post_meta($this->id, 'curlpatterntwo', true);
+    		$curlpatternthree = get_post_meta($this->id, 'curlpatternthree', true);  
+    		$curltimestamp = get_post_meta($this->id, 'curltimestamp', true);
+    		$curlresult = get_post_meta($this->id, 'curlresult', true);
+
+
+		if (!empty($curlurl) && !empty($curlpattern))
+		{
+			if ((time() - $curltimestamp) < 3600) 
+				{      
+				if ((preg_match($curlpattern, $curlresult)))
+				{ return true; }
+				elseif ((preg_match($curlpatterntwo, $curlresult)))
+				{ return true; }
+				elseif ((preg_match($curlpatternthree, $curlresult)))
+				 { return true; }
+				 else
+				{ return false; }
+				} else {
+			$newcurltime = time();
+			update_post_meta($this->id, 'curltimestamp', $newcurltime);
+                	$ch = curl_init();
+                	curl_setopt($ch, CURLOPT_URL, $curlurl);
+                	curl_setopt($ch, CURLOPT_HEADER, FALSE);
+                	curl_setopt($ch, CURLOPT_NOBODY, FALSE);
+                	curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+                	$curlhtml = curl_exec($ch);
+                	curl_close($ch);
+			$curlsubject = $curlhtml;
+			update_post_meta($this->id, 'curlresult', $curlhtml);
+			if (preg_match($curlpattern, $curlsubject))
+			   { return true; }
+			elseif ((preg_match($curlpatterntwo, $curlsubject)))
+			   { return true; }
+			elseif ((preg_match($curlpatternthree, $curlsubject)))
+			   { return true; }
+			else
+			   { return false; }
+*/
+ 
 ?>
